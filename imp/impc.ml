48db3c42bd67ca5448882d80d42fc287ab364290
hjbar <hjbar@protonmail.com>
2024-03-06 19:00:15 +0100

    Improve treatment of options with module Arg

--- imp/impc.ml
+++ imp/impc.ml
@@ -1,11 +1,18 @@
 open Format
 open Owi
 
-let () =
-  let args = Sys.argv in
+(* Execute the fichier donné par le chemin en argument *)
+let exec_file path =
+  (* Owi.Cmd_script.cmd profiling debug optimize [ path ] no_exhaustion *)
+  ignore (Owi.Cmd_script.cmd false false false [ path ] false)
 
-  (* Récupération du fichier *)
-  let file = args.(1) in
+(* Execute en mode debug the fichier donné par le chemin en argument *)
+let exec_file_with_debug path =
+  (* Owi.Cmd_script.cmd profiling debug optimize [ path ] no_exhaustion *)
+  ignore (Owi.Cmd_script.cmd false true false [ path ] false)
+
+(* Compile le fichier donné par le nom en argument *)
+let compile_file file =
   let c = open_in file in
 
   (* Lexing & Parsing *)
@@ -26,19 +33,39 @@ let () =
 
   (* Formatage *)
   let path = Fpath.v output_file in
-  ignore (Owi.Cmd_fmt.cmd true [ path ]);
+  ignore (Owi.Cmd_fmt.cmd true [ path ])
 
-  (* Exécution *)
-  if
-    Array.length args > 2
-    && (Array.mem "--exec" args || Array.mem "--debug" args)
-  then begin
-    let profiling = false in
-    let debug = Array.mem "--debug" args in
-    let optimize = false in
-    let no_exhaustion = false in
-    ignore (Owi.Cmd_script.cmd profiling debug optimize [ path ] no_exhaustion)
-  end;
+(* Compile le fichier donné par le nom en argument, puis l'execute *)
+let compile_file_with_exec file =
+  compile_file file;
+  let output_file = Filename.chop_suffix file ".imp" ^ ".wat" in
+  let path = Fpath.v output_file in
+  exec_file path
 
-  (* Fin *)
-  exit 0
+(* Compile le fichier donné par le nom en argument, puis l'execute en mode debug *)
+let compile_file_with_debug file =
+  compile_file file;
+  let output_file = Filename.chop_suffix file ".imp" ^ ".wat" in
+  let path = Fpath.v output_file in
+  exec_file_with_debug path
+
+(* Gere les options + appel et initialise les fonctions ci-dessus *)
+let () =
+  let usage_msg = "\nHelp Message\n\n  compile the given file" in
+  let speclist =
+    [ ( "--run"
+      , Arg.String compile_file_with_exec
+      , "run the file after compiled it" )
+    ; ( "--debug"
+      , Arg.String compile_file_with_debug
+      , "run the file in debug mode after compiled it" )
+    ]
+  in
+  Arg.parse speclist compile_file usage_msg
+
+(* Leve une erreur si il n'a pas de fichier a compiler *)
+let () =
+  if Array.length Sys.argv < 2 then begin
+    eprintf "usage: %s <file>@\n" Sys.argv.(0);
+    exit 1
+  end
